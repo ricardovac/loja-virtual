efd61f1612151172daf3262fb62042a036eedeb7
ricardovac <ricardoviicius@gmail.com>
2023-04-11 18:24:02 -0300

    init: criando projeto

---
+++ backend/src/main/java/com/ricardo/backend/controller/Hello.java
@@ -0,0 +1,16 @@
+package com.ricardo.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+@RequestMapping("/api")
+public class Hello {
+    @GetMapping("/")
+    public String hello() {
+        return "Olá mundo Spring " + new Date();
+    }
+}
